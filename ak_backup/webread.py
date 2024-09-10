d6cc2a04810a6a0d1b9b74eab29a259cb78c7ae8
Ivy Zhu <eurekazhu@gmail.com>
2024-09-09 23:53:06 -0500

    minor bug fixed

--- ak_backup/webread.py
+++ ak_backup/webread.py
@@ -15,11 +15,11 @@ class SaveThread:
         self.tid = tid
         self.authorid = authorid
         self.max_page = self.get_thread_pgnum()
-        client = requests.session()
-        client.headers = {
+        self.client = requests.session()
+        self.client.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
         }
-        client.cookies.update(cookies)
+        self.client.cookies.update(cookies)
 
     # 获取一页帖子的json
     def get_page(self, pgnum: int):
