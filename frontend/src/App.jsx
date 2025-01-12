145c54c269d40576bb36ab86385880a07d082ba0
Ivy Zhu <eurekazhu@gmail.com>
2025-01-11 21:21:15 -0800

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,41 @@ import './App.css'
 import ButtonUsage from './buttonUsage'
 import { useState } from 'react';
 
+function getUrlParam(url, paramName) {
+  // Create a URL object
+  const urlObj = new URL(url);
+  // Use URLSearchParams to get the parameter value
+  return urlObj.searchParams.get(paramName);
+}
+
+
+
 function App() {
   const [link, setLink] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  let isUrlValid = false
+  let tid = 0
+
+  try {
+    tid = getUrlParam(link, "tid")
+    isUrlValid = true
+  } catch(e) {
+    isUrlValid = false
+  }
+
   const theme = createTheme({
     colorSchemes: {
       dark: true,
     },
   });
+
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <h1>Bacupia - NGA 安科备份</h1>
         <TextField label="安科链接" variant="outlined" sx={{width:"300"}} value={link} onChange={event=>{setLink(event.target.value)}}/>
         <br/><br/>
-        <Button>生成备份</Button>
+        <Button disabled={isLoading || (! isUrlValid)} href={`http://api.ivylh03.net:5000/bacupia/${tid}`} onClick={()=>{setIsLoading(true)}}>{isLoading? "正在处理...":(isUrlValid?"生成备份":"链接不完整")}</Button>
       </Container>
     </ThemeProvider>
   )
