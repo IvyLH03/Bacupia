0c7b969367e02354b3aa2c7a98c39418188c0363
Ivy Zhu <eurekazhu@gmail.com>
2025-01-22 23:04:12 -0600

    frontend

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Button, Container, createTheme, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, ThemeProvider } from '@mui/material'
+import { Button, CircularProgress, Container, createTheme, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, ThemeProvider } from '@mui/material'
 import { useEffect, useState } from 'react';
 import '../app.css'
+import DownloadIcon from '@mui/icons-material/Download';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
@@ -21,6 +22,7 @@ function getUrlParam(url, paramName) {
 function Index() {
   const [link, setLink] = useState("")
   const [isLoading, setIsLoading] = useState(false)
+  
   let isUrlValid = false
   let tid = 0
 
@@ -37,22 +39,35 @@ function Index() {
     },
   });
 
-  const handleDownload = (tid) => {
-    setIsLoading(true)
-    fetch(`https://api.ivylh03.net/bacupia/request/${tid}`)
-      // .then( res => res.blob() )
-      // .then( blob => {
-      //   let file = window.URL.createObjectURL(blob);
-      //   window.location.assign(file);
-      //   setIsLoading(false)
-      // })
-      .then(res=> res.json())
-      .then(data=> console.log(data))
+
+  const [availableFileList, setAvailableFileList] = useState([])
+  const [pendingFileList, setPendingFileList] = useState([])
+
+  const refreshFilelist = () => {
+    fetch(`https://api.ivylh03.net/bacupia/archive`)
+    .then(res=>res.json())
+    .then(data=>{
+      setAvailableFileList(data)
+      console.log(data)
+    })
   }
 
-  useEffect(() => {
-    console.log("Loading state changed:", isLoading);
-  }, [isLoading]);
+  useEffect(()=>{
+    refreshFilelist()
+    if(pendingFileList.length > 0) {
+      setTimeout(refreshFilelist, 10000)
+    }
+  },[isLoading])
+
+  // send backup request to backend
+  const handleRequest = (tid) => {
+    setIsLoading(true)
+    fetch(`https://api.ivylh03.net/bacupia/request/${tid}`)
+    .then( res => res.json() )
+    .then( data => {
+      console.log(data)
+    })
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -61,60 +76,101 @@ function Index() {
           <h1>Bacupia - NGA 安科备份</h1>
           <TextField label="安科链接" variant="outlined" sx={{width:"300"}} value={link} onChange={event=>{setLink(event.target.value)}}/>
           <br/><br/>
-          <Button disabled={isLoading || (! isUrlValid)} onClick={() => {handleDownload(tid)}}>{isLoading? "正在处理...":(isUrlValid?"生成备份":"链接不完整")}</Button>
+          <Button disabled={isLoading || (! isUrlValid)} onClick={() => {handleRequest(tid)}}>{isLoading? "正在处理...":(isUrlValid?"生成备份":"链接不完整")}</Button>
         </Container>
         <Container>
-          <FileListComponent/>
+          <FileListComponent availableFileList={availableFileList} pendingFileList={pendingFileList}/>
         </Container>
       </Container>
     </ThemeProvider>
   )
 }
 
-function FileListItem({data}) {
-  return <>
-    {data.name}
-  </>
+// download files from the backend
+const handleDownload = (filename) => {
+  fetch(`https://api.ivylh03.net/bacupia/download/${filename}`)
+  .then( res => res.blob() )
+  .then( blob => {
+    const file = window.URL.createObjectURL(blob);
+    window.location.assign(file);
+  })
 }
 
-function FileListComponent () {
-  const [fileList, setFileList] = useState([])
-  useEffect(()=>{
-    fetch(`https://api.ivylh03.net/bacupia/archive`)
-    .then(res=>res.json())
-    .then(data=>{
-      setFileList(data)
-      console.log(data)
-    })
-  },[])
+
+
+
+function FileListItem({data, available}) {
+  return <TableRow
+    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+  >
+    <TableCell component="th" scope="row">
+      {data.name}
+    </TableCell>
+    <TableCell align="right">{
+      new Date(data.time * 1000).toUTCString()
+    }</TableCell>
+    <TableCell align="right">{
+      available?
+      <IconButton color="primary" onClick={()=>handleDownload(data.name)}>
+        <DownloadIcon fontSize="large" />
+      </IconButton>
+      :
+      <CircularProgress />
+    }</TableCell>
+
+  </TableRow>
+}
+
+function FileListComponent ({availableFileList, pendingFileList}) {
   // return <>
   //   {fileList.map(e => <FileListItem data={e}/>)}
   // </>
-  return <TableContainer component={Paper}>
-  <Table sx={{ minWidth: 650 }} aria-label="simple table">
-    <TableHead>
-      <TableRow>
-        <TableCell>文档名</TableCell>
-        <TableCell align="right">创建时间</TableCell>
-        <TableCell align="right">状态</TableCell>
+  return <>
+    {
+      pendingFileList.length > 0?
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>文档名</TableCell>
+              <TableCell align="right">备份时间</TableCell>
+              <TableCell align="right">状态</TableCell>
 
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {fileList.map((row) => (
-        <TableRow
-          key={row.name}
-          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-        >
-          <TableCell component="th" scope="row">
-            {row.name}
-          </TableCell>
-          <TableCell align="right">{
-            new Date(row.time * 1000).toUTCString()
-          }</TableCell>
-        </TableRow>
-      ))}
-    </TableBody>
-  </Table>
-</TableContainer>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {pendingFileList.map(e => 
+              <FileListItem key={e.name} data={e} available/>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      :
+      <></>
+    }
+    <br/>
+    {
+      availableFileList.length > 0?
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>文档名</TableCell>
+              <TableCell align="right">备份时间</TableCell>
+              <TableCell align="right">状态</TableCell>
+
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {availableFileList.map(e => 
+              <FileListItem key={e.name} data={e} available/>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      :
+      <></>
+    }
+    
+  </>
 }
